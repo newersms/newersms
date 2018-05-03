0800bda52967b66c4b4ad6c77420d305c94ddc7b
jiaoyeye <875804969@qq.com>
2018-05-03 15:14:55 +0800

    这是测试类

---
+++ sms/src/main/java/com/newer/sms/join/Newer.java
@@ -0,0 +1,36 @@
+package com.newer.sms.join;
+
+public class Newer {
+    private  int i;
+    private  int j;
+
+
+    public Newer(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
+    @Override
+    public String toString() {
+        return "Newer{" +
+                "i=" + i +
+                ", j=" + j +
+                '}';
+    }
+}
